32b229febb16d028470703971fd13595e66daed2
AceMouse <humle000@gmail.com>
2022-05-04 13:40:14 +0200

    dummy BestMove

---
+++ ScrabbleBot/BestMove.fs
@@ -0,0 +1,8 @@
+module internal BestMove
+    let startingSquares board = 0
+        
+        
+    let suggestMove (board : Parser.board)= List.ofArray [|((0, 0), (0u,('a', 0 )))|]
+        
+    
+
--- ScrabbleBot/BestMove.fsi
+++ ScrabbleBot/BestMove.fsi
@@ -1,4 +1,4 @@
-module BestMove 
+module internal BestMove 
     val suggestMove : Parser.board -> ((int * int) * (uint32 * (char * int))) list
     
 
