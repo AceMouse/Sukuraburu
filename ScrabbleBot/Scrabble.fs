ed80d0810f3bfc6dd4d877d77775b17fd4c28cd2
AceMouse <humle000@gmail.com>
2022-05-04 14:44:28 +0200

    add support for sending and receiving ClientMessages and ServerMessages NOT updating board :(

--- ScrabbleBot/Scrabble.fs
+++ ScrabbleBot/Scrabble.fs
@@ -29,6 +29,17 @@ module RegEx =
                     ((x |> int, y |> int), (id |> uint32, (c |> char, p |> int)))
                 | _ -> failwith "Failed (should never happen)") |>
         Seq.toList
+    let parseChange ts =
+        let pattern = @"([0-9]+)[ ]" 
+        Regex.Matches(ts, pattern) |>
+        Seq.cast<Match> |> 
+        Seq.map 
+            (fun t -> 
+                match t.Value with
+                | Regex pattern [ id;] ->
+                    id |> uint32
+                | _ -> failwith "Failed (should never happen)") |>
+        Seq.toList
 
  module Print =
 
@@ -49,11 +60,11 @@ module State =
         hand          : MultiSet.MultiSet<uint32>
         playerTurn    : uint32
         playerCount   : uint32
-        forfeited     : uint32 list
+        notForfeited  : bool list
         points        : int list
     }
 
-    let mkState b d pn h pt pc ff p = {board = b; dict = d;  playerNumber = pn; hand = h; playerTurn = pt; playerCount = pc; forfeited = ff; points = p}
+    let mkState b d pn h pt pc nff p = {board = b; dict = d;  playerNumber = pn; hand = h; playerTurn = pt; playerCount = pc; notForfeited = nff; points = p}
 
     let board st         = st.board
     let dict st          = st.dict
@@ -66,55 +77,108 @@ module Scrabble =
     let playGame cstream pieces (st : State.state) =
 
         let rec aux (st : State.state) =
-            Print.printHand pieces (State.hand st)
-
-            // remove the force print when you move on from manual input (or when you have learnt the format)
-            forcePrint "Input move (format '(<x-coordinate> <y-coordinate> <piece id><character><point-value> )*', note the absence of space between the last inputs)\n\n"
-            let input =  System.Console.ReadLine()
-            let move = RegEx.parseMove input
-
-            debugPrint (sprintf "Player %d -> Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
-            send cstream (SMPlay move)
-
+            let move, change =
+                if st.playerNumber = st.playerTurn then
+                    Print.printHand pieces (State.hand st)
+                    forcePrint "Move or Change or Forfeit or Pass?(m|c|f|p)"
+                    let action = System.Console.ReadLine()[0]
+                    // remove the force print when you move on from manual input (or when you have learnt the format)
+                    match action with
+                            | 'm' ->
+                                forcePrint "Input move (format '(<x-coordinate> <y-coordinate> <piece id><character><point-value> )*', note the absence of space between the last inputs)\n\n"
+                                let input =  System.Console.ReadLine()
+                                let move = RegEx.parseMove input
+                                
+                                debugPrint (sprintf "Player %d -> Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
+                                send cstream (SMPlay move)
+                                
+                                debugPrint (sprintf "Player %d <- Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
+                                (Some(move), None)
+                            | 'c' ->
+                                forcePrint "Input change (format '(<id> )*')\n\n"
+                                let input =  System.Console.ReadLine()
+                                let change = RegEx.parseChange input
+                                send cstream (SMChange change)
+                                debugPrint (sprintf "Player %d <- Server:\n%A\n" (State.playerNumber st) change) // keep the debug lines. They are useful.
+                                (None, Some(change))
+                            | 'f' ->
+                                forcePrint "Forfeited!\n\n"
+                                send cstream SMForfeit
+                                (None, None)
+                            | 'p' ->
+                                forcePrint "Passed!\n\n"
+                                send cstream SMPass
+                                (None, None)
+                else
+                    (None, None)
+                    
             let msg = recv cstream
-            debugPrint (sprintf "Player %d <- Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
-
+                        
+            
+            let rec removePieces i hand (ids : uint32 list) =
+                let id = ids[i]
+                if (i < ids.Length-1) then
+                    removePieces (i+1) (MultiSet.removeSingle id hand) ids
+                else
+                    (MultiSet.removeSingle id hand)
+                    
+            let rec addPieces i hand (pieces : (uint32* uint32)List)=
+                let id, amount = pieces[i]
+                //System.Console.WriteLine (sprintf "id %d amount %d" id amount)
+                if (i < pieces.Length-1) then
+                    addPieces (i+1) (MultiSet.add id amount hand) pieces
+                else
+                    (MultiSet.add id amount hand)
+                    
+            let nextPlayer (st:State.state) curPlayer =
+                let playerOrder = List.append (st.notForfeited[(((curPlayer |> int)+1)%(st.playerCount|>int))..]) (st.notForfeited[..(curPlayer |> int)])
+                (curPlayer + ((List.findIndex id playerOrder) |> uint32))%st.playerCount
+                
             match msg with
             | RCM (CMPlaySuccess(ms, points, newPieces)) ->
                 (* Successful play by you. Update your state (remove old tiles, add the new ones, change turn, etc) *)
-                
-                let rec removeUsedPieces i hand=
-                    let id = fst(snd(move[i]))
-                    if (i < newPieces.Length-1) then
-                        removeUsedPieces (i+1) (MultiSet.removeSingle id hand)
-                    else
-                        (MultiSet.removeSingle id hand)
-                let rec addNewPieces i hand=
-                    let id, amount = newPieces[i]
-                    //System.Console.WriteLine (sprintf "id %d amount %d" id amount)
-                    if (i < newPieces.Length-1) then
-                        addNewPieces (i+1) (MultiSet.add id amount hand)
-                    else
-                        (MultiSet.add id amount hand)
-                
-                //System.Console.WriteLine "----------"
-                //System.Console.WriteLine "before"
-                //System.Console.WriteLine st.hand
-                let st' = {st with playerTurn = (st.playerNumber+1u)%st.playerCount; hand = addNewPieces 0 (removeUsedPieces 0 st.hand); points = List.mapi (fun i v -> if i = (st.playerNumber |> int) then v + points else v) st.points}  // This state needs to be updated
-                //System.Console.WriteLine "after"
-                //System.Console.WriteLine st'.hand
-                //System.Console.WriteLine "----------"
+                System.Console.WriteLine "----------"
+                System.Console.WriteLine "before"
+                System.Console.WriteLine st.hand
+                let reducedHand = removePieces 0 st.hand (List.map (fun x -> x |> snd |> fst) ms)
+                let newHand = addPieces 0 (reducedHand) newPieces
+                let st' = {st with playerTurn = nextPlayer st st.playerNumber; hand = newHand; points = List.mapi (fun i v -> if i = (st.playerNumber |> int) then v + points else v) st.points}  // This state needs to be updated
+                System.Console.WriteLine "after"
+                System.Console.WriteLine st'.hand
+                System.Console.WriteLine "----------"
                 aux st'
             | RCM (CMPlayed (pid, ms, points)) ->
                 (* Successful play by other player. Update your state *)
-                let st' = {st with playerTurn = (pid+1u)%st.playerCount; points = List.mapi (fun i v -> if i = (pid |> int) then v + points else v) st.points}  // This state needs to be updated
+                let st' = {st with playerTurn = nextPlayer st pid; points = List.mapi (fun i v -> if i = (pid |> int) then v + points else v) st.points}  // This state needs to be updated
+                aux st'
+            | RCM (CMPassed pid) ->
+                let st' = {st with playerTurn = nextPlayer st pid}  // This state needs to be updated
+                aux st'
+            | RCM (CMChange(pid, numTiles)) ->
+                let st' = {st with playerTurn = nextPlayer st pid}  // This state needs to be updated
+                aux st'
+            | RCM (CMChangeSuccess newTiles) ->
+                System.Console.WriteLine "----------"
+                System.Console.WriteLine "before"
+                System.Console.WriteLine st.hand
+                let reducedHand = removePieces 0 st.hand change.Value
+                let newHand = addPieces 0 (reducedHand) newTiles
+                let st' = {st with playerTurn = nextPlayer st st.playerNumber; hand = newHand}  // This state needs to be updated
+                System.Console.WriteLine "after"
+                System.Console.WriteLine st'.hand
+                System.Console.WriteLine "----------"
+                aux st'
+            | RCM (CMForfeit pid) ->
+                let st' = {st with playerTurn = nextPlayer st pid; notForfeited = List.mapi (fun i v -> if i = (pid |> int) then false else v) st.notForfeited }  // This state needs to be updated
                 aux st'
             | RCM (CMPlayFailed (pid, ms)) ->
                 (* Failed play. Update your state *)
                 let st' = st // This state needs to be updated
                 aux st'
+            | RCM (CMTimeout pid) ->
+                let st' = {st with playerTurn = nextPlayer st pid}  // This state needs to be updated
+                aux st'
             | RCM (CMGameOver _) -> ()
-            | RCM a -> failwith (sprintf "not implmented: %A" a)
             | RGPE err -> printfn "Gameplay Error:\n%A" err; aux st
 
 
@@ -144,5 +208,5 @@ module Scrabble =
                   
         let handSet = List.fold (fun acc (x, k) -> MultiSet.add x k acc) MultiSet.empty hand
 
-        fun () -> playGame cstream tiles (State.mkState board dict playerNumber handSet playerTurn numPlayers list.Empty (List.ofArray (Array.zeroCreate (numPlayers |> int))))
+        fun () -> playGame cstream tiles (State.mkState board dict playerNumber handSet playerTurn numPlayers (List.init (numPlayers |> int) (fun _ -> true)) (List.ofArray (Array.zeroCreate (numPlayers |> int))))
         
