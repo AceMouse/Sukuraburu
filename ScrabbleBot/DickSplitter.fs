14a583d24c0eab3fdaf5a1a5ca4ce64bd3fa8cf6
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-05-11 14:06:24 +0200

    Fix return type of DickSplitter & use recursion

--- ScrabbleBot/DickSplitter.fs
+++ ScrabbleBot/DickSplitter.fs
@@ -1,17 +1,22 @@
 module ScrabbleBot.DickSplitter
 
-open System.Collections.Generic
 open System.IO
 
 // Splits a dictionary into multiple dictionaries by length and point values
 
-let splitDictionary dic =
-    let splitDic = Dictionary<int, Dictionary.Dict>()
-    File.ReadLines dic |> Seq.iter (fun x ->
-        for i in 0 .. x.Length do
-            let v = splitDic.TryGetValue i
-            if fst v
-            then splitDic.Add (i, (Dictionary.insert x (snd v)))
-            else splitDic.Add (i, Dictionary.insert x (Dictionary.empty ()))
-        )
-    splitDic
+let splitDictionary dic : Dictionary.Dict list =
+    let rec aux (dic : string list) (tl : Map<int, Dictionary.Dict>) =
+        match dic with
+        | x::xs -> 
+            let v = tl.TryGetValue x.Length
+            let rec insert w (d : Map<int, Dictionary.Dict>) acc =
+                match acc with
+                | 0 -> d
+                | _ ->
+                    if fst v
+                    then insert w (d.Add (x.Length, Dictionary.insert w (snd v))) (acc - 1)
+                    else insert w (d.Add (x.Length, Dictionary.insert w (Dictionary.empty ()))) (acc - 1)
+            let res = insert x tl x.Length
+            aux xs res
+        | [] -> tl.Values |> List.ofSeq
+    aux (File.ReadLines dic |> List.ofSeq) Map.empty
